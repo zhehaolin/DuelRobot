b2a09b12431b9104eb134faad2c2dd1d298e5d9f
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-13 11:47:14 -0400

    Robot

--- DuelRobot/src/main/Robot.java
+++ DuelRobot/src/main/Robot.java
@@ -1,5 +1,14 @@
 package main;
 
-public class Robot {
+public interface Robot {
 
+	
+	//print a taunt
+	//later: the taunt must be different each time the method is called
+	void taunt();
+	
+	//returns the name of the Dueler
+	String getName();
+	
+	
 }
